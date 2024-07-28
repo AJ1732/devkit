2b2b8f21cd3a46eb3a85a09f5630a9438c45b8a0
AJ1732 <ejemeniboi@gmail.com>
2024-07-28 11:01:52 +0100

    chore: setup project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="content-grid">
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          300: "#3A0A66",
+          500: "#270753",
+          700: "#170342",
+        },
+        tint: {
+          100: "#F3F6FF",
+          200: "#E3E3F6",
+        },
+        fade: {
+          100: "rgba(39, 7, 83, 0.50)",
+          200: "rgba(23, 3, 66, 0.75)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
